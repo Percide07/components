625d90bd4562d60c65c9d68703335889c86ae20c
Percide07 <percidehonhonou@gmail.com>
2023-09-25 00:25:59 +0100

    Adding the news features for the and styling the display of the project

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import DataCars from './components/dataCars.js';
 function App() {
 
   return (
-    <>
+    <div className='display'>
       <Card name={DataCars[0].name} type={DataCars[0].type} gasolineLiter={DataCars[0].gasolineLiter} kindOfTransition={DataCars[0].kindOfTransition} people={DataCars[0].people} pricePerDay={DataCars[0].pricePerDay} img={DataCars[0].img} />
-    </>
+      <Card name={DataCars[1].name} type={DataCars[1].type} gasolineLiter={DataCars[1].gasolineLiter} kindOfTransition={DataCars[1].kindOfTransition} people={DataCars[1].people} pricePerDay={DataCars[1].pricePerDay} img={DataCars[1].img} />
+      <Card name={DataCars[2].name} type={DataCars[2].type} gasolineLiter={DataCars[2].gasolineLiter} kindOfTransition={DataCars[2].kindOfTransition} people={DataCars[2].people} pricePerDay={DataCars[2].pricePerDay} img={DataCars[2].img} />
+      <Card name={DataCars[3].name} type={DataCars[3].type} gasolineLiter={DataCars[3].gasolineLiter} kindOfTransition={DataCars[3].kindOfTransition} people={DataCars[3].people} pricePerDay={DataCars[3].pricePerDay} img={DataCars[3].img} />
+      <Card name={DataCars[4].name} type={DataCars[4].type} gasolineLiter={DataCars[4].gasolineLiter} kindOfTransition={DataCars[4].kindOfTransition} people={DataCars[4].people} pricePerDay={DataCars[4].pricePerDay} img={DataCars[4].img} />
+      <Card name={DataCars[5].name} type={DataCars[5].type} gasolineLiter={DataCars[5].gasolineLiter} kindOfTransition={DataCars[5].kindOfTransition} people={DataCars[5].people} pricePerDay={DataCars[5].pricePerDay} img={DataCars[5].img} />
+      <Card name={DataCars[6].name} type={DataCars[6].type} gasolineLiter={DataCars[6].gasolineLiter} kindOfTransition={DataCars[6].kindOfTransition} people={DataCars[6].people} pricePerDay={DataCars[6].pricePerDay} img={DataCars[6].img} />
+      <Card name={DataCars[7].name} type={DataCars[7].type} gasolineLiter={DataCars[7].gasolineLiter} kindOfTransition={DataCars[7].kindOfTransition} people={DataCars[7].people} pricePerDay={DataCars[7].pricePerDay} img={DataCars[7].img} />
+    </div>
   )
 }
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,40 @@
-import '../components/Card.css'
+import '../components/Card.css';
+import photo1 from '../images/gas.png';
+import photo2 from '../images/manual.png';
+import photo3 from '../images/people.png';
+import heart from '../images/heart.png';
 const Card = ({name,type,gasolineLiter,kindOfTransition,people,pricePerDay,img})=> {
     return(
         <>
+
             <article className="Card">
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{type}</h2>
+
+                <div className='carName'>
+                    {name}
+                    <img src={heart} alt="like image" className='like'/>
                 </div>
-                <div>
-                    <img src= {img} alt="Car's photo" />
+                <div className='carType'>{type}</div>
+                <div className='pict'>
+                    <img src= {img} alt="Car's photo" className='image1'/>
                 </div>
-                <div>
-                    <p>{gasolineLiter}</p>
-                    <p>{kindOfTransition}</p>
-                    <p>{people}</p>
-                </div>
-                <div>
-                    <p>{pricePerDay}</p>
-                </div>
-                <button>Rent Now</button>
+                <section className='part1'>
+                    <div className='gasP'>
+                        <img src={photo1} alt="gas quantity" className='image2'/>
+                        <div className='desc'>{gasolineLiter}</div>
+                    </div>
+                    <div className='transP'>
+                        <img src={photo2} alt="transmission" className='image3'/>
+                        <div className='mec'>{kindOfTransition}</div>
+                    </div>
+                    <div className='peoP'>
+                        <img src={photo3} alt="people" className='image4'/>
+                        <div className='place'>{people}</div>
+                    </div>
+                </section>
+                <section className='part2'>
+                    <div className='price'>{pricePerDay}</div>
+                    <button className='btn'>Rent Now</button>
+                </section>
 
             </article>
         </>
